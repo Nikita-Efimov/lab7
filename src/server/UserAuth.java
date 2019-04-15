6ed859c177a2559ff523268265bea0e9891c9cc2
nikita-efimov <nikikot@list.ru>
2019-04-15 22:51:30 +0300

    added password generator

--- src/server/UserAuth.java
+++ src/server/UserAuth.java
@@ -9,7 +9,25 @@ class UserAuth {
 
         SecureRandom rand = new SecureRandom();
         rand.setSeed(Clock.systemUTC().millis());
-        // System.out.println(rand.next(10));
+
+        String password = "";
+        byte[] bytes = new byte[7];
+        rand.nextBytes(bytes);
+
+        final byte CAPITAL_CHAR_OFFSET = 'Z' - 'A' - 1;
+        final byte ORDINARY_CHAR_OFFSET = 'z' - 'a' - 1;
+
+        for (byte b : bytes) {
+            byte[] symbolByte = new byte[1];
+            rand.nextBytes(symbolByte);
+            switch ((byte)(b & 0xfffl) % 2) {
+                case 0: password += (char)((symbolByte[0] & 0xfffl) % CAPITAL_CHAR_OFFSET + (byte)'A'); break;
+                case 1: password += (char)((symbolByte[0] & 0xfffl) % ORDINARY_CHAR_OFFSET + (byte)'a'); break;
+                default: password += (symbolByte[0] & 0xfffl) % 10; break;
+            }
+        }
+
+        System.out.println(password);
     }
 
     public UserAuth(final String login, final String password) {
