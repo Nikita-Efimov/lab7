b98198f7f67486d679c1891d035ad8acbe699642
manat <manatov2000@yandex.ru>
2019-04-20 06:24:01 +0700

    added some database functions

--- src/server/DatabaseInteraction.java
+++ src/server/DatabaseInteraction.java
@@ -1,3 +1,6 @@
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.sql.*;
 
 interface DBUserInteractionable {
@@ -20,13 +23,18 @@ class DatabaseInteraction implements DBUserInteractionable, DBCityCollection {
     private static final String DB_CONNECTION = "jdbc:postgresql://localhost:5432/lab7";
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "root";
+
+    //private static final String DB_PASSWORD = "admin";
+    //private static final String DB_USER = "postgres";
+    //private static final String DB_CONNECTION = "jdbc:postgresql://127.0.0.1:5432/laba7DB";
+
     private Connection dbConnection;
 
     public DatabaseInteraction() {
         getDBConnection();
         try {
             activateQuery("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, login TEXT NOT NULL UNIQUE, password TEXT NOT NULL);");
-            activateQuery("CREATE TABLE IF NOT EXISTS objects (id int4 NOT NULL, parent_id int4 REFERENCES users(id) ON DELETE CASCADE, PRIMARY KEY (id, parent_id));");
+            activateQuery("CREATE TABLE IF NOT EXISTS objects (id int4 NOT NULL, parent_id int4 REFERENCES users(id) ON DELETE CASCADE, json TEXT , PRIMARY KEY (id, parent_id));");
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -98,30 +106,224 @@ class DatabaseInteraction implements DBUserInteractionable, DBCityCollection {
     @Override
     public void add(City city) {
 
+        PreparedStatement statement = null;
+        try {
+            statement = dbConnection.prepareStatement("INSERT INTO objects (json, parent_id) VALUES (?, ?)");
+
+            JSONObject json= getJsonObject(city);
+
+
+            statement.setString(1, json.toString());
+        statement.setInt(2, 0);
+        statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeFirst() {
+
+
+        PreparedStatement statement = null;
+        try {
+            statement = dbConnection.prepareStatement("DELETE FROM objects [WHERE parent_id=?] LIMIT 1;");
+
+
+            statement.setInt(1, 0);
+            statement.execute();
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void remove(City city) {
 
+        PreparedStatement statement = null;
+        try {
+            statement = dbConnection.prepareStatement("DELETE FROM objects [WHERE json=? AND parent_id=?];");
+
+            JSONObject json = getJsonObject(city);
+
+            statement.setString(1, json.toString());
+            statement.setInt(2, 0);
+            statement.execute();
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
     public String show() {
-        return "";
+        PreparedStatement statement = null;
+        String returnString="";
+        try {
+        statement = dbConnection.prepareStatement("SELECT * FROM objects;");
+
+        statement.execute();
+
+
+            ResultSet result = statement.executeQuery();
+
+
+            while (result.next()){
+                returnString+= processInput(result.getString("json")).toString()+"\n";
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return returnString;
     }
 
     @Override
     public boolean addIfMax(City city) {
+
+
+        PreparedStatement statement = null;
+
+        try {
+            statement = dbConnection.prepareStatement("SELECT * FROM objects WHERE parent_id=?;");
+            statement.setInt(1, 0);
+            statement.execute();
+
+
+            ResultSet result = statement.executeQuery();
+
+            City maxcity=null;
+            City curcity=null;
+            while (result.next()){
+            curcity=processInput(result.getString("json"));
+            if(curcity.compareTo(maxcity)>0) maxcity=curcity ;
+
+            }
+
+            if(city.compareTo(maxcity)>0) {
+                JSONObject json = getJsonObject(city);
+
+
+                statement = dbConnection.prepareStatement("INSERT INTO objects (json, parent_id) VALUES (?, ?)");
+                statement.setString(1, json.toString());
+                statement.setInt(2, 0);
+                statement.execute();
+                return true;
+            }
+
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
         return false;
+    }
+
+    private JSONObject getJsonObject(City city) {
+        JSONObject json = new JSONObject();
+        json.put("name",city.name);
+        json.put("areaSize",city.areaSize);
+        json.put("initDate",city.initDate);
+        json.put("x",city.x);
+        json.put("y",city.y);
+        return json;
     }
 
     @Override
     public boolean removeLower(City city) {
+
+        PreparedStatement statement = null;
+
+        try {
+            statement = dbConnection.prepareStatement("SELECT * FROM objects WHERE parent_id=?;");
+            statement.setInt(1, 0);
+            statement.execute();
+
+            ResultSet result = statement.executeQuery();
+            statement = dbConnection.prepareStatement("DELETE FROM objects [WHERE id=IN (?)]");
+
+
+            String ids="";
+            City curcity=null;
+            while (result.next()){
+                curcity=processInput(result.getString("json"));
+
+                if(city.compareTo(curcity)>0) {
+                    JSONObject json = getJsonObject(city);
+                    ids+=","+result.getInt("id");
+
+                }
+
+            }
+            ids=ids.substring(1);
+
+            statement.setString(1, ids);
+            statement.execute();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return false;
     }
+
+
+    protected City processInput(String jsonInput) {
+        City city = null;
+        String name;
+        Integer size, x, y;
+
+        if (jsonInput.indexOf('}') < jsonInput.length() - 1)
+            throw new RuntimeException("uncorrect command");
+
+        try {
+            JSONObject obj = new JSONObject(jsonInput);
+
+            try {
+                name = obj.getString("name");
+            } catch (JSONException e) {
+                name = "Unnamed";
+            }
+
+            try {
+                size = obj.getInt("size");
+            } catch (JSONException e) {
+                size = 0;
+            }
+
+            try {
+                x = obj.getInt("x");
+            } catch (JSONException e) {
+                x = 0;
+            }
+
+            try {
+                y = obj.getInt("y");
+            } catch (JSONException e) {
+                y = 0;
+            }
+
+            city = new City(name, size, x, y);
+
+        } catch (JSONException e) {
+            throw new RuntimeException("uncorrect command");
+        }
+
+        return city;
+    }
+
+
 }
+
